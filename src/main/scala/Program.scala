c1011667e5adde4f469a0b4fa60c7bbe2feb35a8
Eric Winer <enwiner@gmail.com>
2013-07-27 00:17:53 -0400

    re-pushing the spherocontroller

--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -2,11 +2,17 @@ import com.leapmotion.leap.{Listener, Controller}
 import se.nicklasgavelin.bluetooth.Bluetooth
 
 object Program {
+
   def main(args: Array[String]) {
-    val sphero = SpheroController.connect()
+    println("Hello!  Connecting to Sphero...")
+    val sphero: Sphero = SpheroController.connect()
+    calibrate(sphero)
+    println("Connected to Sphero!  Connecting to Leap...")
     val leapController = new Controller()
     leapController.addListener(new LeapListener(sphero))
     readLine("Press enter to exit.")
   }
 
+  def calibrate(controller: Sphero) {
+  }
 }
--- src/main/scala/SpheroController.scala
+++ src/main/scala/SpheroController.scala
@@ -1,23 +1,62 @@
 import java.util
+import java.util.concurrent.CountDownLatch
 import se.nicklasgavelin.bluetooth.Bluetooth.EVENT
 import se.nicklasgavelin.bluetooth.{BluetoothDiscoveryListener, BluetoothDevice, Bluetooth}
-import se.nicklasgavelin.sphero.command.RollCommand
 import se.nicklasgavelin.sphero.exception.{RobotBluetoothException, InvalidRobotAddressException}
 import se.nicklasgavelin.sphero.Robot
 
-class SpheroController extends Sphero {
+class SpheroController(robot: Robot) extends Sphero {
 
-  private var maybeRobot: Option[Robot] = None
-  def robot = maybeRobot.getOrElse(throw new IllegalStateException("Robot not initialized!"))
+  // e.g.
+  def roll(heading: Int) {
+    robot.roll(0, 0)
+  }
 
-  private val btListener = new BluetoothDiscoveryListener {
+  def roll(velocity: Float, heading: Float) {}
+
+  def stop() {}
+
+  def beginCalibration() {}
+
+  def acceptCalibration() {}
+
+  def setColor(r: Int, g: Int, b: Int) {}
+}
+
+object SpheroController {
+
+  def connect() = {
+    val holder = new RobotHolder
+    val bt = new Bluetooth(new RobotBtListener(holder), Bluetooth.SERIAL_COM)
+    bt.discover()
+    val robot = holder.awaitRobot()
+    new SpheroController(robot)
+  }
+
+  private class RobotHolder {
+    private var robot: Option[Robot] = None
+    private val foundSignal = new CountDownLatch(1)
+
+    def awaitRobot() = {
+      foundSignal.await()
+      robot.getOrElse(throw new IllegalStateException("Didn't find robot in the robot holder!"))
+    }
+
+    def storeRobot(r: Robot) {
+      robot = Some(r)
+      foundSignal.countDown()
+    }
+  }
+
+  private class RobotBtListener(holder: RobotHolder) extends BluetoothDiscoveryListener {
     def deviceSearchCompleted(devices: util.Collection[BluetoothDevice]) {
       println("Bluetooth search completed.")
     }
 
     def deviceDiscovered(device: BluetoothDevice) {
       if (Robot.isValidDevice(device)) {
-        connect(device)
+        println(s"Found robot ${device.getAddress}")
+        createRobot(device, holder)
       }
     }
 
@@ -28,13 +67,12 @@ class SpheroController extends Sphero {
     }
   }
 
-  private def connect(device: BluetoothDevice) {
-    println(s"Found robot ${device.getAddress}")
+  private def createRobot(device: BluetoothDevice, holder: RobotHolder) {
     try {
       val r = new Robot(device)
       if (r.connect()) {
         println(s"Connected to ${device.getName} : ${device.getAddress}!")
-        maybeRobot = Some(r)
+        holder.storeRobot(r)
       } else {
         println("Failed to connect to robot!")
       }
@@ -49,28 +87,4 @@ class SpheroController extends Sphero {
     }
   }
 
-  {
-    val bt = new Bluetooth(btListener, Bluetooth.SERIAL_COM)
-    bt.discover()
-  }
-
-  def roll(velocity: Float, heading: Float) {
-    robot.roll(heading, velocity)
-  }
-
-  def stop() {
-    robot.stopMotors()
-  }
-
-  def beginCalibration() {
-
-  }
-
-  def acceptCalibration() {
-
-  }
-
-  def setColor(r: Int, g: Int, b: Int) {
-    robot.setRGBLEDColor(r, g, b)
-  }
 }
