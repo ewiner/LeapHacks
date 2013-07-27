044d8bd22a2e107dca5a90c954c11d1715606260
Vipul Shekhawat <vish092@gmail.com>
2013-07-27 04:16:32 -0400

    cleaning up and fixing Program.scala

--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -2,8 +2,6 @@ import com.leapmotion.leap.Controller
 
 object Program {
 
-
-
   def main(args: Array[String]) {
     println("Hello!  Connecting to Sphero...")
     val sphero: Sphero = SpheroController.connect().getOrElse{
@@ -15,23 +13,15 @@ object Program {
     println("Connected to Sphero!  Connecting to Leap...")
 
     val leapController = new Controller()
-    if (leapController.isConnected) {
-      val listener = new LeapListener(sphero)
-      leapController.addListener(listener)
-      readLine("Press enter to exit.")
-      leapController.removeListener(listener)
-    } else {
-      println("Leap not connected...")
-      readLine("Press enter to roll a little, then exit.")
-      sphero.roll(1, 0)
-      Thread.sleep(500)
-    }
+    //TODO: check if leap is actually connected
+    val listener = new LeapListener(sphero)
+    leapController.addListener(listener)
+    readLine("Press enter to exit.")
+
     sphero.disconnect()
   }
 
   def calibrate(controller: Sphero) {
     println("Calibrating")
   }
-
-  // sudo ln -sfh OLD Current
 }
